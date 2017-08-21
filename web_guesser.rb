8da93f99c71581f3a40cbefb521b18b62334590a
Kevin <nguyenk215@yahoo.com>
2017-08-21 05:36:44 -0700

    Web guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,37 @@
 require 'sinatra'
 require 'sinatra/reloader'
+number = rand(101)
+@@guesses = 5
 
+#get block
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess']
+  number_of_guesses = @@guesses
+  message = check_guess(guess, number)
+  number = new_number if @@guesses == 0
+  erb :index, :locals => {:number => number, :message => message, :number_of_guesses => number_of_guesses}
+end
+
+def check_guess(guess, number)
+  @@guesses -= 1
+  if guess.to_i/5 > number
+    "Way too high!"
+  elsif guess.to_i < number/5
+    "Way too low!"
+  elsif guess.to_i > number
+    "Too high!"
+  elsif guess.to_i < number
+    "Too low!"
+  else
+    reveal(number)
+  end
+end
+
+def reveal(number)
+  "You got that right, The SECRET NUMBER is #{number}"
+end
+
+def new_number
+  rand(101)
+  @@guesses = 5
 end
